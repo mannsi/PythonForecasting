75f244dde67fbb534b6de813d4fad7f4fc565b1a
mannsi <gudbjorn.einarsson@gmail.com>
2017-03-26 13:39:43 +0000

    Remove unused runner file

--- forecast/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `seq 1 129`;
-do
-        echo "Running item number" $i
-        /home/manns/anaconda3/envs/tensor_flow_env_35/bin/python /home/manns/PycharmProjects/PythonForecasting/forecast/main.py ${i}
-done
